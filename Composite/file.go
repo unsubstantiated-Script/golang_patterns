0fdbf24ec54f8f2a43e2bd56a79412d5ed4aaf33
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2023-06-10 16:20:47 -0700

    composite finished

---
+++ Composite/file.go
@@ -0,0 +1,16 @@
+package Composite
+
+import "fmt"
+
+type File struct {
+	name string
+}
+
+func (f *File) search(keyword string) {
+	fmt.Printf("Searching for keyword %s in file %s\n", keyword, f.name)
+}
+
+func (f *File) getName() string {
+	return f.name
+}
+
