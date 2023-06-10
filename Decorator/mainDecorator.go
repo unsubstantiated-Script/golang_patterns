957b302dc0510f421bded8ebe45f403c88082e3f
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2023-06-10 16:27:54 -0700

    Decorator done

--- Decorator/mainDecorator.go
+++ Decorator/mainDecorator.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func MainDecorator() {
 	fmt.Println("Hello Decorator")
+
+	pizza := &VeggieMania{}
+
+	//Add Cheese
+	pizzaWithCheese := &CheeseTopping{
+		pizza: pizza,
+	}
+
+	//Add tomato
+	pizzaWithCheeseAndTomato := &TomatoTopping{
+		pizza: pizzaWithCheese,
+	}
+
+	fmt.Printf("Price of a Veggie Mania with Cheese and Tomato is %d\n", pizzaWithCheeseAndTomato.getPrice())
 }
