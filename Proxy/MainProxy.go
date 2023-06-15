d03a9580e9f20fa90e1d552ab060cb6f84d327e9
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2023-06-14 21:09:03 -0700

    Proxy finished

--- Proxy/MainProxy.go
+++ Proxy/MainProxy.go
@@ -4,4 +4,24 @@ import "fmt"
 
 func MainProxy() {
 	fmt.Println("Hello Proxy!")
+
+	nginxServer := newNginxServer()
+	appStatusURL := "/app/status"
+	createuserURL := "/create/user"
+
+	httpCode, body := nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(createuserURL, "POST")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(createuserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
 }
